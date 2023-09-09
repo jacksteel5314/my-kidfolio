1e3694cfbebca28cb0030da3040fc3008a367b54
Jack Steel <jas333@duke.edu>
2023-09-09 00:28:19 -0400

    Frontpage complete

--- src/frontpage.js
+++ src/frontpage.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -6,7 +5,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1 id="ttl1">My Kidfolio</h1>
-        <input type="text" value="Enter Name" id="textbox" onclick="takename()"></input>
+        <img class="img1" src="https://clipart-library.com/images/rcnKKqndi.png" width="150" height="150"></img>
+        <div>
+          <input type="text" value="Enter Name" id="textbox"></input>
+          <input type="button" value="Start" id="infbox" onclick="start()"></input>
+        </div>
         <input type="button" value="Learn More" id="infbox" onclick="learnmore()"></input>
       </header>
     </div>
