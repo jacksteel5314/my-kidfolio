f11d6bc9bf77b802e1f0460594afe8a34df3146d
Jack Steel <jas333@duke.edu>
2023-09-08 23:32:40 -0400

    First Upload

--- src/App.js
+++ src/App.js
@@ -5,18 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 id="ttl1">My Kidfolio</h1>
+        <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fclipart-library.com%2Fmoney-bag-picture.html&psig=AOvVaw3QNCdOJtiWlSHmBHHcA5d7&ust=1694316480674000&source=images&cd=vfe&opi=89978449&ved=0CA8QjRxqFwoTCMDupvHKnIEDFQAAAAAdAAAAABAl"></img>
+        <input type="text" value="Enter Name" id="textbox" onclick="takename()"></input>
+        <input type="button" value="Learn More" id="infbox" onclick="learnmore()"></input>
       </header>
     </div>
   );
