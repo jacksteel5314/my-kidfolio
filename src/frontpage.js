f2769386663805291ec3f41e2b79d05ad94cec23
Jack Steel <jas333@duke.edu>
2023-09-09 01:20:46 -0400

    Editing Changes

--- src/frontpage.js
+++ src/frontpage.js
@@ -1,19 +1,34 @@
-import './App.css';
+import './frontpage.css';
 
-function App() {
+function frontpage() {
   return (
     <div className="App">
       <header className="App-header">
         <h1 id="ttl1">My Kidfolio</h1>
         <img class="img1" src="https://clipart-library.com/images/rcnKKqndi.png" width="150" height="150"></img>
         <div>
-          <input type="text" value="Enter Name" id="textbox"></input>
-          <input type="button" value="Start" id="infbox" onclick="start()"></input>
+          <input type="text" placeholder="Enter Name" id="textbox" minlength="2" maxlength="15"></input>
+          <button id="infbox" onclick="start()">
+            Start
+          </button>
         </div>
-        <input type="button" value="Learn More" id="infbox" onclick="learnmore()"></input>
+        <div class="container">
+          <button type="submit" id="infbox" onclick="openPopup()">
+            Learn More
+          </button>
+        </div>
       </header>
     </div>
   );
 }
+let popup = document.getElementById("popup");
 
-export default App;
+function openPopup(){
+  popup.classList.add("open-popup");
+}
+
+function closePopup(){
+  popup.classList.remove("open-popup");
+}
+
+export default frontpage;
